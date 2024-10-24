7d0d235aa24e30bacf83bec9a63091425a9223a4
stephenkwok85 <stephenkwok85@gmail.com>
2024-10-24 19:52:04 -0400

    Updated GUI

--- src/main/java/com/example/demo/ClinicManagerController.java
+++ src/main/java/com/example/demo/ClinicManagerController.java
@@ -9,15 +9,19 @@ public class ClinicManagerController {
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private ComboBox<String> myComboBox;
 
     @FXML
-    private ComboBox<String> myComboBox;
+    private ComboBox<String> display_selector;
 
     @FXML
     public void initialize() {
         myComboBox.getItems().addAll("X RAY", "ULTRA SOUND", "CATSCAN");
+        display_selector.getItems().addAll("Display office appointments", "Display imaging appointments", "Display all appointments by date", "Display all appointments by patient", "Display all appointments by county", "Display billing statements for all patients", "Display all credit amounts for providers");
+
+        display_selector.setOnAction(e -> {
+            String selectedOption = display_selector.getValue();
+        });
     }
+
 }
