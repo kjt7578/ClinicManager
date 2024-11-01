c568e865472cc4e020a790ae9441216bf928e3d0
stephenkwok85 <stephenkwok85@gmail.com>
2024-11-01 13:36:43 -0400

    removed managermain comments

--- src/main/java/com/example/demo/ClinicManagerMain.java
+++ src/main/java/com/example/demo/ClinicManagerMain.java
@@ -7,22 +7,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-/**
- * The ClinicManagerMain class serves as the entry point for the Clinic Manager application.
- * It initializes the JavaFX application and loads the main user interface.
- *
- * @author Stephen Kwok and Jeongtae Kim
- */
 public class ClinicManagerMain extends Application {
-
-    /**
-     * The start method is called when the JavaFX application is launched.
-     *
-     * It initializes the main stage, loads the FXML layout, and sets the scene for the application.
-     *
-     * @param stage the primary stage for this application, onto which the application scene can be set
-     * @throws IOException if the FXML file cannot be loaded
-     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ClinicManagerMain.class.getResource("clinic-view.fxml"));
@@ -32,13 +17,6 @@ public class ClinicManagerMain extends Application {
         stage.show();
     }
 
-    /**
-     * The main method serves as the entry point for the Java application.
-     *
-     * It launches the JavaFX application by invoking the launch method.
-     *
-     * @param args command-line arguments passed to the application
-     */
     public static void main(String[] args) {
         launch();
     }
